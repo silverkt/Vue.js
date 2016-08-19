24c9b34bd27a47fda28d4ccb715f7cab2d129cf3
silver.sun <itisagift@yeah.net>
2016-08-19 18:07:34 +0800

    ref

---
+++ 111.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+func abc(ab:Int, cd:Int)->Void {
+
+}
+
+
+abc(ab:1,cd:2)
--- test/test/SecondPageView.swift
+++ test/test/SecondPageView.swift
@@ -11,5 +11,22 @@ import UIKit
 
 class SecondPageView: UIView
 {
-    
+    init() {
+        super.init(frame: UIScreen.mainScreen().bounds)
+        self.backgroundColor = UIColor.blackColor()
+        let button1: UIButton = UIButton(type: .System)
+        button1.frame = CGRect(x: 20, y: 50, width: 150, height: 80)
+        //UIButton(frame: CGRect(x: 20, y: 50, width: 150, height: 80))
+        //button1.backgroundColor = UIColor.blackColor()
+        button1.setTitle("dianji", forState: .Normal)
+        
+        
+        self.addSubview(button1)
+        
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }

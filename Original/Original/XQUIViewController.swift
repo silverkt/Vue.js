dceb8c52bc328eb00bdc3082495eb4fab3417ccc
silver.sun <itisagift@yeah.net>
2016-08-22 18:05:33 +0800

    new one

---
+++ Original/Original/XQUIViewController.swift
@@ -0,0 +1,15 @@
+//
+//  XQController.swift
+//  Original
+//
+//  Created by tsing.sun on 16/8/22.
+//  Copyright © 2016年 tsing.sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class XQUIViewController: UIViewController
+{
+    
+}

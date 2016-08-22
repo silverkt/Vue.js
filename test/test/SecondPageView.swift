48c607af125a5971ed0e1d476a59cc79604cf08e
silver.sun <itisagift@yeah.net>
2016-08-22 15:27:25 +0800

    test is over

--- test/test/SecondPageView.swift
+++ test/test/SecondPageView.swift
@@ -19,25 +19,25 @@ class SecondPageView: UIView
         //UIButton(frame: CGRect(x: 20, y: 50, width: 150, height: 80))
         //button1.backgroundColor = UIColor.blackColor()
         button1.setTitle("dianji", forState: .Normal)
-        let fc: NSObject? = FirstPageController()
-        button1.addTarget(fc, action:Selector("btnclick"), forControlEvents: .TouchUpInside)
+       
+        button1.addTarget(self, action:Selector("btnclick"), forControlEvents: .TouchUpInside)
         
         self.addSubview(button1)
         
         
     }
     
-//    func btnclick()->Void{
-//        let superv: UIView? = self.superview
-//        let lb:UILabel? = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 20))
-//        lb?.text = "sss"
-//        self.addSubview(lb!)
-//       
-//        self.removeFromSuperview()
-//        let secv:UIView = FirstPageView()
-//        superv?.addSubview(secv)
-//        
-//    }
+    func btnclick()->Void{
+        let superv: UIView? = self.superview
+        let lb:UILabel? = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 20))
+        lb?.text = "sss"
+        self.addSubview(lb!)
+       
+        self.removeFromSuperview()
+        let secv:UIView = FirstPageView()
+        superv?.addSubview(secv)
+        
+    }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")

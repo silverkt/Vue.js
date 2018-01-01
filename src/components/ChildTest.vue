32e7a70b6f63c071cf023904fd510c2ca9ede254
silver.sun <itisagift@yeah.net>
2018-01-01 23:10:41 +0800

    test elementUI & axios

--- src/components/ChildTest.vue
+++ src/components/ChildTest.vue
@@ -1,18 +1,36 @@
 <template>
-	<div>this is a child component+{{msg}}</div>
+	<div>this is a child component+{{msg}}++<br>{{resData.title}}<br>{{resData.value}}<br>{{resData.unit}}<br>
+
+
+	<el-button type="primary" size="mini" @click="btnclicked($event)">测试按钮</el-button>
+
+	</div>
 </template>
 <script>
 export default {
 	name: 'child-one',
 	data() {
 		return {
-			msg: "child shit"
+			msg: "child shit",
+			resData: ''
 		}
 	},
 	created() {
 		console.log("child created");
 	},
 	methods: {
+		btnclicked: function($event) {
+			console.log($event);
+			console.log('button.clicked');
+
+			let res = this.$axios.get('http://ceos.webcity3d.com/images/rems/circle4.json');
+			res.then((data) => {
+				console.log(data);
+				console.log(data.data);
+				this.resData = data.data;
+			});
+		}
+
 
 	}
 }

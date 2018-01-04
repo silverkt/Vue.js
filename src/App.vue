1d9c2125f90e1d240392b5edd4bf7bb4799776d2
silver.sun <itisagift@yeah.net>
2018-01-04 23:00:28 +0800

    navi menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
+
 <template>
   <div id="app">
+  <div class="line"></div>
+<el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1"> <router-link to="/">首页</router-link></el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="3"> <router-link to="/mytest" >测试页</router-link></el-menu-item>
+</el-menu>
+
     <img src="./assets/logo.png">
-    <router-link to="/">首页</router-link>
-    <router-link to="/mytest">测试页</router-link>
+
+
+
+   
+ 
+   
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+      data() {
+        return {
+            name: 'app',
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+      },
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+}
+.el-menu a:link{
+  text-decoration: none;
 }
 </style>

8db00acfabe22885a5864cd3fd5e4159f2419051
silver.sun <itisagift@yeah.net>
2018-01-08 16:14:24 +0800

    step line

--- src/components/NewRouter.vue
+++ src/components/NewRouter.vue
@@ -6,6 +6,17 @@
 	<el-button @click="changeswitch()">change</el-button>
 	<router-view/>
 	{{loadswitch}}
+
+
+<div id="stepsm">
+	<el-steps :active="1">
+  <el-step title="步骤 1" icon="el-icon-edit"></el-step>
+  <el-step title="步骤 2" icon="el-icon-upload"></el-step>
+  <el-step title="步骤 3" icon="el-icon-picture"></el-step>
+   </el-steps>
+</div>
+
+
 	</div>
 
 </template>
@@ -33,5 +44,7 @@ export default {
 }
 </script>
 <style scoped>
-	
+	#stepsm {
+		width:500px;
+	}
 </style>

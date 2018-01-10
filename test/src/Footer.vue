972d67cab9f44274f63adf16d73de003d574d477
silver.sun <itisagift@yeah.net>
2018-01-11 07:01:46 +0800

    test for packjs called for component

---
+++ test/src/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+ footer
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      naviMenuObj: ''
+    }
+  },
+  methods:{
+     
+  },
+  created() {
+    this.naviMenuObj = ennNaviMenuConfig;
+    console.log(this);
+  },
+  mounted() {
+ 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
---
+++ test/static/enn.navi.menu.js
@@ -0,0 +1,21 @@
+    	var ennNaviMenu =  [
+        { "id":"1", "target":"_self", "title":"首页21323132", "link":"/abcad"},
+        { "id":"2", "target":"_self", "title":"首页1", "link":"/ab"},
+        { "id":"3", "target":"_self", "title":"首页2", "link":"/abc"},
+        { "id":"4", "target":"_self", "title":"首页3", "link":"/abca"},
+        { "id":"5", "target":"_blank", "title":"首页4", "link":"http://www.baidu.com"}
+
+      ];
+     var subEnnNaviMenu =  [
+        { "id":"s1", "target":"_self", "title":"首页21323132", "link":"/abcad"},
+        { "id":"s2", "target":"_self", "title":"首页1", "link":"/ab"},
+        { "id":"s3", "target":"_self", "title":"首页2", "link":"/abc"},
+        { "id":"s4", "target":"_self", "title":"首页3", "link":"/abca"},
+        { "id":"s5", "target":"_blank", "title":"首页4", "link":"http://www.baidu.com"}
+      ];
+
+      var ennNaviMenuConfig = {
+      	"flag": "0",
+      	"ennNaviMenu": ennNaviMenu,
+      	"subEnnNaviMenu": subEnnNaviMenu
+      } 

ffac261172d4b9bfcf2ad9f3dc6557062205ad9f
silver.sun <itisagift@yeah.net>
2018-04-17 16:42:21 +0800

    axios all spread

--- src/components/AxiosTest.vue
+++ src/components/AxiosTest.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>{{msg}}
+		{{axiosRs.data.otherjson.other1}}
 	<el-button @click="request">request</el-button>
 	</div>
 </template>
@@ -7,12 +8,26 @@
 export default {
 	data() {
 		return {
-			msg: 'msg'
+			msg: 'msg',
+			axiosRs: {
+					  "data": {
+					    "code": "o",
+					    "name": "1212",
+					    "otherjson": {
+					      "other1": "value0",
+					      "other2": "value0"
+					    }
+					  }
+					}
 		}
 	},
 	methods: {
 		request: function() {
-			this.msg ="哈哈哈哈哈"
+			this.msg ="哈哈哈哈哈";
+			this.$axios.get('https://www.easy-mock.com/mock/59a8d3424006183e48ef9507/myproj/test').then( res => {
+				console.log(res.data);
+				this.axiosRs = res.data;
+			})
 		}
 	}
 
